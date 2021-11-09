b86b08ec8bf0b03d6a6df34742b8f76935466d17
mg4145 <mg4145@columbia.edu>
2021-11-08 23:03:24 -0500

    Finished create_group, include database stmt l8r

--- User.py
+++ User.py
@@ -6,7 +6,18 @@ def User():
         self.current_group = "" 
 
 
-    #def create_group(self):
+    def create_group(self):
+        group = input("Please enter the name of your group.\nGroup names are limited to alphanumeric characters ([a-z], [A-Z], [0-9]): ")
+        valid = False
+        while valid is False: 
+            while group.isalnum() is False:
+                group = input("Invalid characters used, please try again.") #Check if it's database or not.
+            if group != "charmander": #replace charmander with the names in database - change this later!
+                print("Group successfully created!")
+                valid = True
+            else:
+                group = input("Group name is already in database, try again: ")
+            
 
     #def add_user(self):
 
@@ -26,10 +37,8 @@ def User():
 
     #def options(self): 
 
-
     '''Not for first iteration'''
     #def leave_group(self):
     #def current_group(self):
     #def display_reviews_for_restaurant():
-        # displays reviews for restaurant from all friend groups that user is a part of
-
+        # displays reviews for restaurant from all friend groups that user is a part ofd
