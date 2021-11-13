f332f4b1d14a97e788d50ff85749c5c6337d53f0
Rashel Rojas <rdr2139@columbia.edu>
2021-11-13 16:59:03 -0500

    Ignore

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template, request, redirect, jsonify, make_response
+from flask import Flask, render_template, request, redirect, jsonify
+from flask import make_response
 from json import dumps
 import db
-
+import logging
 
 app = Flask(__name__)
 
-import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+
+
+'''
+Implement the '/' endpoint
+Method Type: GET
+Intial Webpage for restaurants initialized
+'''
+
+
+@app.route('/')
+def index():
+    db.init_db()
+    return 'Server initialized!'
+
 
 '''
 Implement '/readreviews' endpoint
 Method Type: POST
 '''
+
 
 @app.route('/readreviews', methods=['GET'])
 def read_reviews_restaurant():
@@ -21,11 +36,21 @@ def read_reviews_restaurant():
     reviews = db.get_review_by_name(res_name)
 
     return jsonify(restaurant_name=res_name, reviews=reviews)
- 
 
 
+'''
+Implement localhost:5000/readreviews?resName=val1&stars=val2' endpoint
+Method Type: POST
+'''
 
 
+@app.route('/readreviews', methods=['GET'])
+def star_reviews():
+
+    stars = request.args.get('stars')
+    star_reviews = db.get_all_reviews_above_rating(stars)
+
+    return jsonify(reviews=star_reviews)
 
 
 if __name__ == '__main__':
