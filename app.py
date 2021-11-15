846689a5723545ee6bd323f619fd2f986c037ea0
Rashel Rojas <rdr2139@columbia.edu>
2021-11-15 01:40:24 -0500

    Added get_all_reviews_for_restaurant_given_rating() call to /readreviews endpoint

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-
+'''
+Homepage
+'''
 @app.route('/', methods=['GET'])
 def index():
     db.clear()
@@ -17,23 +19,31 @@ def index():
     return 'Server Works!'
 
 
+'''
+Endpoint:  /readreviews?restaurant=___&stars=___
+UI:        User fills out a form with their query 
+Returns:   reviews that match that query
+'''
 @app.route('/readreviews', methods=['GET'])
 def read_reviews_restaurant():
-    res_name = request.args.get('resName')
+    res_name = request.args.get('restaurant')
     rating = request.args.get('stars')
 
-    if res_name is not None and rating is None:     # query: all reviews for a specific rest.
-        reviews = db.get_review_by_name(res_name)
-        return jsonify(resName=res_name, reviews=reviews)
-    elif res_name is None and rating is not None:   # query: all reviews above a certain rating
-        reviews = db.get_all_reviews_above_rating(rating)
+    # query: all reviews for a specific restaurant
+    if res_name is not None and rating is None:     
+        reviews = db.get_all_reviews_for_restaurant(res_name)
+        return jsonify(restaurant=res_name, reviews=reviews)
+    # query: all reviews at/above a specific rating
+    elif res_name is None and rating is not None:   
+        reviews = db.get_all_reviews_given_rating(rating)
         return jsonify(reviews=reviews)
-    elif res_name is not None and rating is not None:   # query: all reviews for a spec. restaurant above a certain rating
-        #reviews = db.get_all_reviews_by_name_and_above_rating(res_name, rating) TO DO: add this func to db.py
-        #return jsonify(resName=res_name, reviews=reviews)
-        pass
-    else: # error: no parameters passed in
-        return jsonify(resName = "", reviews="")
+    # query: all reviews for a specific restaurant at/above a specific rating
+    elif res_name is not None and rating is not None:   
+        reviews = db.get_all_reviews_for_restaurant_given_rating(res_name, rating) 
+        return jsonify(restaurant=res_name, reviews=reviews)
+    # error: empty parameters
+    else: 
+        return jsonify(error="No parameters for query were entered.")
  
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
