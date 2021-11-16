29bfcd93c15d18bab791566582386762c936da94
daisyye0730 <72574086+daisyye0730@users.noreply.github.com>
2021-11-15 21:45:03 -0500

    changes made to master

--- db.py
+++ db.py
@@ -18,9 +18,9 @@ def init_db():
             PRIMARY KEY(UNI))""")
         cur = conn.cursor()
 
-        reviews_file = open("review.csv")
-        rows = csv.reader(reviews_file)
-        cur.executemany("INSERT INTO REVIEWS VALUES (?, ?, ?, ?)", rows)
+        # reviews_file = open("review.csv")
+        # rows = csv.reader(reviews_file)
+        # cur.executemany("INSERT INTO REVIEWS VALUES (?, ?, ?, ?)", rows)
 
         #uni_file = open("uni.csv")
         #rows = csv.reader(uni_file)
@@ -47,15 +47,11 @@ def get_all_reviews_for_restaurant(res_name):
     try:
         conn = sqlite3.connect('Lion_Eats')
         cur = conn.cursor()
-        cur.execute("""SELECT * FROM REVIEWS""")
+        cur.execute("SELECT * FROM REVIEWS WHERE restaurant_name=?", res_name)
         rows = cur.fetchall()
-        result = []
-        for i in rows:
-            if i[0] == res_name:
-                result.append(i)
         conn.commit()
         print('Database Online, get reviews for a restaurant')
-        return result
+        return rows
 
     except Error as e:
         print(e)
@@ -78,15 +74,11 @@ def get_all_reviews_given_rating(rating):
     try:
         conn = sqlite3.connect('Lion_Eats')
         cur = conn.cursor()
-        cur.execute("SELECT * FROM REVIEWS")
+        cur.execute("SELECT * FROM REVIEWS where star >= ?", (rating,))
         rows = cur.fetchall()
-        result = []
-        for i in rows:
-            if i[1] >= rating:
-                result.append(i)
         conn.commit()
         print('Database Online, get reviews at/above a rating')
-        return result
+        return rows
     except Error as e:
         print(e)
         return None
@@ -131,15 +123,11 @@ def get_restaurants_above_ratings(rating):
     try:
         conn = sqlite3.connect('Lion_Eats')
         cur = conn.cursor()
-        cur.execute("with avg_table as (select restaurant_name, avg(star) as avg_star_rating from REVIEWS group by restaurant_name) select * from avg_table")
+        cur.execute("with avg_table as (select restaurant_name, avg(star) as avg_star_rating from REVIEWS group by restaurant_name) select * from avg_table where avg_star_rating >= ?", rating)
         rows = cur.fetchall()
-        result = []
-        for r in rows: 
-            if r[-1] >= int(rating):
-                result.append(r)
         conn.commit()
-        print('Database Online, get reviews above restaurant\'s average rating')
-        return result
+        print('Database Online, get reviews above restaurant\'s average rating rating')
+        return rows
     except Error as e:
         print(e)
         return None
@@ -161,15 +149,13 @@ def get_review(res_name, uni):
     try:
         conn = sqlite3.connect('Lion_Eats')
         cur = conn.cursor()
-        cur.execute("SELECT * FROM REVIEWS where restaurant_name=? AND UNI=?", (res_name.lower(), uni.lower()))
+        cur.execute("SELECT * FROM REVIEWS where restaurant_name=? AND UNI=?", (res_name, uni))
         row = cur.fetchone()
-        print(row)
         conn.commit()
         print('Database Online, get review')
         return row
     except Error as e:
         print(e)
-        return None
 
     finally:
         if conn:
@@ -189,10 +175,10 @@ def edit_review(UNI, res_name, new_rating, new_review):
     try:
         conn = sqlite3.connect('Lion_Eats')
         cur = conn.cursor()
-        if new_rating.isnumeric() is False or type(UNI) != str or type(res_name) != str or type(new_review) != str:
-            return None
-        if int(new_rating) > 5:  
-            return None
+        if type(new_rating) != int or type(UNI) != str or type(res_name) != str or type(new_review) != str:
+            return Error
+        if new_rating > 5:
+            return Error
         lower_UNI = UNI.lower()
         lower_res_name = res_name.lower()
         cur.execute("update REVIEWS set star = ?, review = ? where UNI = ? and\
@@ -202,7 +188,6 @@ def edit_review(UNI, res_name, new_rating, new_review):
         print('Database Online, review edited')
     except Error as e:
         print(e)
-        return None
 
     finally:
         if conn:
@@ -225,7 +210,7 @@ def add_review(row):
             return Error
 
         new_row = (row[0].lower(), row[1], row[2], row[3].lower())
-        if type(new_row[0]) != str or new_row[1].isnumeric() is False or type(new_row[2]) != str or type(new_row[3]) != str:
+        if type(new_row[0]) != str or type(new_row[1]) != int or type(new_row[2]) != str or type(new_row[3]) != str:
             return Error
         cur.execute(
             "INSERT INTO REVIEWS (restaurant_name, star, review, UNI) VALUES\
@@ -234,7 +219,6 @@ def add_review(row):
         print('Database Online, review added')
     except Error as e:
         print(e)
-        return None
 
     finally:
         if conn:
