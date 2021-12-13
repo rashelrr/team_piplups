1d918d04bb4b779072ee0d638931db99dedacef5
Rashel Rojas <rdr2139@columbia.edu>
2021-12-13 00:15:07 -0500

    fixed app.py

--- app.py
+++ app.py
@@ -87,10 +87,11 @@ Adds review to database
 
 @app.route('/addreview', methods=['POST'])
 def add_review():
-    name = request.args.get('restaurant')
-    star = request.args.get('stars')
-    comment = request.args.get('review')
-    uni = request.args.get('user')
+    entry = request.get_json(force=True)
+    name = entry['restaurant']
+    star = entry['stars']
+    comment = entry['review']
+    uni = entry['user']
 
     result = db.get_review_uni_res(name, uni)
     if len(result) == 0:
